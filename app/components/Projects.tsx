79e63e8da008b3c8f8428db529d2f1e6e9bb3bd2
Umut Çalışkan <umutcaliskan@Mac.bbrouter>
2025-03-12 02:30:36 +0300

    First Commit

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -319,7 +319,7 @@ const websites = [
     description: "Oto kurtarma firması için geliştirilen kurumsal websitesi. 7/24 çekici hizmeti rezervasyonu yapılabilen modern bir arayüz.",
     screenshot: "/cvgen-screenshot.jpg",
     technologies: ["Opencart", "PHP", "MySQL", "OpenCart Extensions" , "Twig", "cPanel"],
-    type: "Google Chrome Extension",
+    type: "Website",
     icon: (
       <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
