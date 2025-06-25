b7f5fd24bf3b0e3961d8080e39a091680f8aeea7
Umut Çalışkan <umutcaliskan@Mac.bbrouter>
2025-06-25 20:47:07 +0300

    new project

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -333,10 +333,10 @@ const websites = [
     name: "Mulky",
     href: "https://mulky.com.tr",
     githubUrl: "",
-    description: "My personal portfolio website showcasing my projects, skills, and experience.",
+    description: "A comprehensive property, tenant, site, and hotel management platform I developed during my time at Finis Software, offering efficient solutions for real estate and hospitality operations.",
     screenshot: "/mulky.png",
     technologies: ["Django", "React Native & Expo", "Nativewind", "TypeScript"],
-    type: "Websit and Mobile",
+    type: "Website and Mobile",
     icon: (
       <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
